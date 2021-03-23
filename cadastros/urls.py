1ce98c1ce93fd3dc964cc1a610d9c7c8988b6620
Rafael Henrique Dalegrave Zottesso <rafael.zottesso@ifpr.edu.br>
2021-03-22 22:39:17 -0300

    Correção do models.

--- cadastros/urls.py
+++ cadastros/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import EstadoCreate, CidadeCreate, EmailSuportCreate, FornecedorCreate, FuncionarioCreate, PrefixoMaquinaCreate, ProdutoCreate
+from .views import EstadoCreate, CidadeCreate, FornecedorCreate, FuncionarioCreate, MaquinaCreate, ProdutoCreate
 # from .views import NomeCreate
 # from .views import NomeUpdate   
 # from .views import NomeDelete
@@ -11,34 +11,14 @@ urlpatterns = [
 
     ####################### Adicionar ###################################
 
-    path('cadastrar/estados', EstadoCreate.as_view(), 
-        name='cadastrar-estado'),
-    path('cadastrar/produto', ProdutoCreate.as_view(), 
-        name='cadastrar-produto'),
-    path('cadastrar/cidade', CidadeCreate.as_view(),
-         name='cadastrar-cidade'),
-    path('cadastrar/funcionario', FuncionarioCreate.as_view(),
-         name='cadastrar-funcionario'),
-    path('cadastrar/fornecedor', FornecedorCreate.as_view(),
-         name='cadastrar-fornecedor'),
-    path('cadastrar/maquina', PrefixoMaquinaCreate.as_view(),
-         name='cadastrar-prefixomaquina'),
-    path('cadastrar/EmailSuport', EmailSuportCreate.as_view(),
-         name='cadastrar-EmailSuport'),
+    path('cadastrar/estados', EstadoCreate.as_view(), name='cadastrar-estado'),
+    path('cadastrar/produto', ProdutoCreate.as_view(), name='cadastrar-produto'),
+    path('cadastrar/cidade', CidadeCreate.as_view(), name='cadastrar-cidade'),
+    path('cadastrar/funcionario', FuncionarioCreate.as_view(), name='cadastrar-funcionario'),
+    path('cadastrar/fornecedor', FornecedorCreate.as_view(), name='cadastrar-fornecedor'),
+    path('cadastrar/maquina', MaquinaCreate.as_view(), name='cadastrar-prefixomaquina'),
 
-    
-
-    
-
-
-
-
-
-
-
-
-
-
+     ## todos updade
 
 
 ]
--- cadastros/views.py
+++ cadastros/views.py
@@ -1,6 +1,6 @@
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.list import ListView
-from .models import Estado, EmailSuport, Fornecedor, Funcionario, Cidade, PrefixoMaquina, Produto
+from .models import Estado, Fornecedor, Funcionario, Cidade, Maquina, Produto
 from django.urls import reverse_lazy
 
 # Create your views here.
@@ -10,23 +10,36 @@ class FornecedorCreate(CreateView):
     fields = ["cnpj", "nomedocontribuinte", "nome",
               "email", "telefoneCelular", "telefoneFixo", "site"]
     template_name = "cadastros/form.html"
+    
+    # Tem que ter em todos inserir, alterar e excluir
+    success_url = reverse_lazy("nome_da_url_no_urls.py")
+
+
 class FuncionarioCreate(CreateView):
     model = Funcionario
     fields = ["nome", "dataNascimento", "setor",
               "telefoneCelular", "telefoneFixo", "cpf", "rg", "email", "cnh", "cargo"]
     template_name = "cadastros/form.html"
+
+
 class CidadeCreate(CreateView):
     model = Cidade
     fields = ["nome"]
     template_name = "cadastros/form.html"
+
+
 class EstadoCreate(CreateView):
     model = Estado
     fields = ["nome","sigla"]
     template_name = "cadastros/form.html"
-class PrefixoMaquinaCreate(CreateView):
-    model = PrefixoMaquina
+
+
+class MaquinaCreate(CreateView):
+    model = Maquina
     fields = ["nome", "tipo", "ano", "horimetro", "prefixoMaquina"]
     template_name = "cadastros/form.html"
+
+
 class ProdutoCreate(CreateView):
     model = Produto
     fields = ["nome", "qtdeatual", "qtdemin", "precoProduto"]
@@ -34,13 +47,9 @@ class ProdutoCreate(CreateView):
 
     
 
-
+"""
 class EmailSuportCreate(CreateView):
     model = EmailSuport
     fields = ["nome","email", "telefoneFixo", "telefoneCelular", "descricao"]
     template_name = "cadastros/form.html"
-    
-    
-
-
-
+"""
