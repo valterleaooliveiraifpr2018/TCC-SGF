d1eb37b6bb795b10b4abf32a28760f5ff137f0f6
leodiceo <leodiceo@gmail.com>
2021-04-06 14:35:21 -0300

    up banco

--- cadastros/admin.py
+++ cadastros/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-from .models import Produtos_Saida, Produtos_Entrada, Revisao
+from .models import Cidade, Entrada, Fornecedor, Funcionario, Maquina, Produto, Produtos_Saida, Produtos_Entrada, Revisao, Saida
 # Register your models here.
 
 admin.site.register(Produtos_Entrada)
 admin.site.register(Produtos_Saida)
+admin.site.register(Cidade)
+admin.site.register(Entrada)
+admin.site.register(Fornecedor)
+admin.site.register(Funcionario)
+admin.site.register(Maquina)
+admin.site.register(Produto)
+admin.site.register(Saida)
 admin.site.register(Revisao)
